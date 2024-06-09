c1e9dc0c814d4ce64ad55437788fa6639b006716
Kai Williams <kailuwilliams@gmail.com>
2024-06-08 21:34:20 -0700

    Add route to process and convert video to 360p resolution using ffmpeg

--- video-processing-server/src/index.ts
+++ video-processing-server/src/index.ts
@@ -2,13 +2,36 @@ import express from "express"
 import ffmpeg from "fluent-ffmpeg"
 
 const app = express()
+app.use(express.json())
 
-const port = 3000
+app.post("/process-video", (req, res) =>{
+  //Get path of the input video
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+  //Check if there were no parameters recieved
+  if(!inputFilePath){
+    res.status(400).send("Bad Request: Missing input file path.")
+  }
+  if (!outputFilePath) {
+    res.status(400).send("Bad Request: Missing output file path.")
+  }
+
+  //Convert video
+  //maybe change 360 to 480 or 720 later
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") //360p  resolution
+    .on("end", () => {
+      res.status(200).send("Processing finished successfully")
+    })
+    .on("error", (err) => {
+      console.log(`An error has occurred: ${err.message}`)
+      res.status(500).send(`Internal Server Error ${err.message}`)
+    })
+    .save(outputFilePath)
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`)
 })
