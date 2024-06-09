e88e9fbcb5745c8bb8507bb25338dd9b3ef3c58c
Kai Williams <kailuwilliams@gmail.com>
2024-06-08 17:31:06 -0700

    set up dependencies

---
+++ video-processing-server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import ffmpeg from "fluent-ffmpeg"
+
+const app = express()
+
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`Video processing service listening at http://localhost:${port}`)
+})
+
+
