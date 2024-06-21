be59c76cddfbfafb5e823d1ba27b7546d45516a4
Kai Williams <kailuwilliams@gmail.com>
2024-06-20 21:08:40 -0700

    check if file has name

--- video-processing-server/src/index.ts
+++ video-processing-server/src/index.ts
@@ -1,22 +1,26 @@
 import express from "express"
 import ffmpeg from "fluent-ffmpeg"
+import { setupDirectories } from "./storage"
+
+setupDirectories()
 
 const app = express()
 app.use(express.json())
 
 app.post("/process-video", (req, res) =>{
-  //Get path of the input video
-  const inputFilePath = req.body.inputFilePath
-  const outputFilePath = req.body.outputFilePath
-
-  //Check if there were no parameters recieved
-  if(!inputFilePath){
-    res.status(400).send("Bad Request: Missing input file path.")
+  //Get the bucket and filename from the cloub pub/sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8')
+    data = JSON.parse(message)
+    if (!data.name) {
+      throw new Error('Invalid messgae payload received.')
+    }
   }
-  if (!outputFilePath) {
-    res.status(400).send("Bad Request: Missing output file path.")
+  catch (error) {
+    console.log(error)
+    return res.status(400).send('Bad Request: missing filename')
   }
-
 })
 
 const port = process.env.PORT || 3000
