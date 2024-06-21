9a8b19e69640505d874677d03242678109f6963a
Kai Williams <kailuwilliams@gmail.com>
2024-06-20 20:54:38 -0700

    create delte file functions

--- video-processing-server/src/storage.ts
+++ video-processing-server/src/storage.ts
@@ -78,3 +78,56 @@ export async function uploadProcessedVideo(fileName: string) {
 
   await bucket.file(fileName).makePublic()
 }
+
+
+/**
+ * Deletes raw file from local
+ *
+ * @param fileName - The name of the file to delete
+ * from the {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+
+export function deleteRawVideo(fileName: string) {
+  return deleteFile(`${localRawVideoPath}/${fileName}`)
+}
+
+
+/**
+ * Deletes processed file from local
+ *
+ * @param fileName - The name of the file to delete
+ * from the {@link localProcessedVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted
+ */
+
+export function deleteProcessedVideo(fileName: string) {
+  return deleteFile(`${localProcessedVideoPath}/${fileName}`)
+}
+
+
+/**
+ * @param filePath - The path of the file to delete
+ * @returns A promise that resolves when the file has been deleted
+ */
+
+function deleteFile(filePath: string): Promise<void> {
+  return new Promise((resolve, reject) =>{
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) =>{
+        if (err) {
+          console.log(`Failed to delete file at ${filePath}`, err)
+          reject(err)
+        }
+        else {
+          console.log(`File deleted at ${filePath}.`)
+          resolve()
+        }
+      })
+    }
+    else {
+      console.log(`File not found at ${filePath}, skipping the delete.`)
+      resolve()
+    }
+  })
+}
