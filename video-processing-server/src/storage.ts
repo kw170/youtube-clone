8ab9a8fb6e2341459ad3d34a7ae92395ad8fe473
Kai Williams <kailuwilliams@gmail.com>
2024-06-28 15:57:56 -0700

    fix logging spacing

--- video-processing-server/src/storage.ts
+++ video-processing-server/src/storage.ts
@@ -58,7 +58,7 @@ export async function downloadRawVideo(fileName: string){
   )
 
   console.log(
-    `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/ ${fileName}.`
+    `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}.`
   )
 }
 
